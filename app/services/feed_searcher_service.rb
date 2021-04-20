635189405c54da15eab730635adb897d9e4328ff
Çağrı Özkan <cagrio.dev@gmail.com>
2021-04-20 21:02:34 +0300

    Add feed_searcher_service

---
+++ app/services/feed_searcher_service.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class FeedSearcherService
+  def initialize(query)
+    @query = query
+  end
+
+  def call
+    url = "https://itunes.apple.com/search?term=#{@query}&entity=podcast"
+    response = RequestMakerService.new(url).call
+    format_response(response.payload) if response.success?
+  end
+
+  private
+
+  def format_response(response)
+    results = []
+    response = JSON.parse(response)
+    response['results'].each do |r|
+      result = { 'rss_url': r['feedUrl'],
+                 'pic_url': r['artworkUrl600'],
+                 'provider': r['artistName'],
+                 'name': r['trackName'],
+                 'external_id': r['collectionId'].to_s
+                 }
+      results << result
+    end
+    results
+  end
+end
