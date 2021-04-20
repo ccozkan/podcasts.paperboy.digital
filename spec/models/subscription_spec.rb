5a4434dfdaba49b62135c59c6e550c5b5d9127c9
Çağrı Özkan <cagrio.dev@gmail.com>
2021-04-20 20:57:43 +0300

    Add model consistency tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'model consistency' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:feed) }
+  end
 end
