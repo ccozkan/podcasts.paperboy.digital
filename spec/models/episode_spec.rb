9cf74b856040c17ed9c5f234b630cb21b6911850
Çağrı Özkan <cagrio.dev@gmail.com>
2021-04-20 22:34:13 +0300

    Fix uniqueness validation test

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Episode, type: :model do
   describe 'model consistency' do
+    let(:feed) { create(:feed) }
+    let!(:episode) { create(:episode, feed_id: feed.id) }
+
     it { is_expected.to belong_to(:feed) }
     it { is_expected.to validate_presence_of(:external_id) }
     it { is_expected.to validate_uniqueness_of(:external_id) }
