cafd4b2c614b0e1732c2aba176c87330b989b6bb
Çağrı Özkan <cagrio.dev@gmail.com>
2021-04-20 22:33:55 +0300

    Update factories

--- spec/factories/episodes.rb
+++ spec/factories/episodes.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :episode do
-    media_url { "MyString" }
-    title { "MyString" }
-    body { "MyText" }
-    subscription { nil }
+    audio_url { "https://url_of_episode_audio.mp3" }
+    title { "Cool Episode" }
+    summary { "Stuff covered on this cool episode" }
+    external_id { 'abc123' }
+    published_at { Time.current - 3.day }
   end
 end
--- spec/factories/feeds.rb
+++ spec/factories/feeds.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :feed do
-    rss_url { "MyString" }
-    external_id { "MyString" }
-    name { "MyString" }
+    rss_url { "https://url_of_feed_rss.rss" }
+    pic_url { "https://url_of_feed_pic.png" }
+    external_id { "xyz123" }
+    name { "Awesome Feed" }
   end
 end
