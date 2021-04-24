bf9aa91d60ff43a47a96ee44719f30d705d03685
Çağrı Özkan <cagrio.dev@gmail.com>
2021-04-24 20:20:07 +0300

    Add feed_searcher_service tests

---
+++ spec/services/feed_searcher_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe FeedSearcherService, type: :service do
+  describe '#call' do
+    let(:subject) { FeedSearcherService.new('reverberation radio').call }
+
+    it 'should find some shows' do
+      expect(subject.length).to be > 0
+    end
+
+    it 'should return proper format' do
+      expect(subject.class).to eq Array
+      expect(subject.first.class).to eq Hash
+      expect(subject.first[:rss_url]).not_to eq nil
+      expect(subject.first[:external_id]).not_to eq nil
+      expect(subject.first[:pic_url]).not_to eq nil
+      expect(subject.first[:provider]).not_to eq nil
+      expect(subject.first[:name]).not_to eq nil
+    end
+  end
+end
