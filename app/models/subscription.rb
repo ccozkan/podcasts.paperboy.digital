a710e06d867fa4fecbb82488a77a36df53a642a3
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-11 21:57:21 +0300

    Validate user-feed uniqueness

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,5 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :feed
+  validates_uniqueness_of :user_id, scope: :feed_id
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
+  let(:user) { create(:user) }
+  let(:feed) { create(:feed) }
+  let!(:subscription) { create(:subscription, feed_id: feed.id, user_id: user.id) }
+
   describe 'model consistency' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:feed) }
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:feed_id) }
   end
 end
