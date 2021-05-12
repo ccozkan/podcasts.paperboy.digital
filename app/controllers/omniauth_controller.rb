d9988e9e7f1b0ad822b1d5c716ea4fe231a0b4da
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-12 04:15:43 +0300

    Redirect to diffrent path when on signin and signup

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,10 +1,15 @@
 class OmniauthController < ApplicationController
   def github
-    @user = User.create_from_provider_data(request.env['omniauth.auth'])
-    if @user.persisted?
-      redirect_to search_path, notice: "Welcome :^) Let's start subscribing"
-    else
+    @user = User.find_or_create_from_provider_data(request.env['omniauth.auth'])
+
+    if !@user.persisted?
       redirect_to new_user_registration_path, alert: @user.errors.full_messages.first
+    elsif !@user.new_record?
+      sign_in(:user, @user)
+      redirect_to search_path, notice: "Welcome :^)"
+    elsif @user.new_record?
+      sign_in(:user, @user)
+      redirect_to search_path, notice: "Welcome :^) Let's start subscribing"
     end
   end
 end
