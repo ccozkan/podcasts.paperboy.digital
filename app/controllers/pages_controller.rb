428cb21b066f116715cc4f8190e522b1190fa3d8
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-12 04:22:07 +0300

    Simplify pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,4 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  def search
-    @search_results = if search_params[:query]
-                        FeedSearcherService.new(search_params[:query]).call
-                      else
-                        []
-                      end
-  end
-
-  private
-
-  def search_params
-    params.permit(:query)
-  end
+  def home; end
+  def about; end
 end
