8f7c8926d235db60dc6ad6ae20a3afdafa75c2e0
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-17 00:27:40 +0300

    Set sidekiq as queue adapter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.active_job.queue_adapter = :sidekiq
 end
