cd24f30849f5f612e83a4100b1faf0a1e6325a5f
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-16 21:21:49 +0300

    Update redirect on successful save

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -11,19 +11,20 @@ class SubscriptionsController < ApplicationController
 
     @subscription = Subscription.new(user_id: current_user.id, feed_id: feed.id)
     if @subscription.save
-      redirect_to subscriptions_path
+      redirect_to dashboard_path, notice: 'Subscribed!'
     else
       render json: @subscription.errors
     end
   end
 
   def destroy
+    subscription = Subscription.find_by(user_id: current_user.id, )
     feed = Feed.find_by(id: permitted_params[:id])
   end
 
   private
 
   def permitted_params
-    params.permit(:external_id, :name, :pic_url, :rss_url, :provider, :id)
+    params.permit(:external_id, :name, :pic_url, :rss_url, :provider)
   end
 end
