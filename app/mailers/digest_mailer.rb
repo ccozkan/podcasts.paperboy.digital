c6e49c1204d531b939f8e9ee434f02431f655004
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-18 00:45:06 +0300

    Limit digest to 20

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -1,7 +1,7 @@
 class DigestMailer < ApplicationMailer
   def weekly_digest(user_id)
     @user = User.find_by(user_id)
-    @episodes = @user.last_weeks_episodes.limit(100)
+    @episodes = @user.last_weeks_episodes.limit(20)
     mail(to: @user.email, subject: 'Your Weekly Podcast Digest')
   end
 end
