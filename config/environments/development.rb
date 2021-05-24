8affa25f184dd54ae473fbe3fbb4a955973cb799
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-24 03:08:51 +0300

    Update queue adapter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,8 +66,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.active_job.queue_adapter = :sidekiq
-
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
