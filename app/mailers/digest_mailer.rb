add40593b7fe283cf7f4c0a1b9244b6a2cf32025
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-26 16:50:31 +0300

    Dont send email if there is no new episodes for that user

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -2,6 +2,9 @@ class DigestMailer < ApplicationMailer
   def weekly(user_id)
     @user = User.find_by(id: user_id)
     @episodes = @user.last_weeks_episodes.sample(3)
-    mail(to: @user.email, subject: 'Your Weekly Podcast Digest')
+
+    unless @episodes.blank?
+      mail(to: @user.email, subject: '🎧🚲🗞Your Weekly Podcast Digest')
+    end
   end
 end
