bae4c4e8403082631562ff3b48041b47120f3678
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-30 22:19:04 +0300

    Add tests for model consistency and .dismiss_an_episode

--- spec/models/interaction_spec.rb
+++ spec/models/interaction_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Interaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:feed) { create(:feed) }
+  let!(:episode) { create(:episode, feed_id: feed.id) }
+
+  describe 'model consistency' do
+    let!(:interaction) { create(:interaction, user_id: user.id, episode_id: episode.id)}
+
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:episode) }
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:episode_id) }
+  end
+
+  describe '.dismiss_an_episode' do
+    it { expect(user.interactions).to eq [] }
+    it do
+      Interaction.dismiss_an_episode(episode.id, user.id)
+
+      expect(user.interactions).not_to eq []
+      expect(user.interactions.first.episode_id).to eq episode.id
+      expect(user.interactions.first.dismissed).to eq true
+    end
+  end
 end
