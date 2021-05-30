f73c8ec79149b11e0d71237f4e28fb19953234ea
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-30 22:09:28 +0300

    Use create! on .dismiss_an_episode

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -4,6 +4,6 @@ class Interaction < ApplicationRecord
   validates_uniqueness_of :user_id, scope: :episode_id
 
   def self.dismiss_an_episode(episode_id, user_id)
-    Interaction.create(dismissed: true, episode_id: episode_id, user_id: user_id)
+    Interaction.create!(dismissed: true, episode_id: episode_id, user_id: user_id)
   end
 end
