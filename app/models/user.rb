bb81c35ba941cb8c84905dde79f0913e5682f921
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-31 21:37:30 +0300

    Add dependent reltaionships, refactor .find_or_create_from_provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,28 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
          :omniauthable, omniauth_providers: [:github]
 
-  has_many :subscriptions
-  has_many :interactions
+  has_many :subscriptions, dependent: :destroy
+  has_many :interactions, dependent: :destroy
   has_many :feeds, through: :subscriptions
   has_many :episodes, through: :interactions
 
   def self.find_or_create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_initialize do |user|
+    user = where(provider: provider_data.provider,
+                 uid: provider_data.uid).first_or_initialize
+    if user.new_record?
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.confirm if user.new_record?
+      user.confirm
+      user.save!
     end
+    user
   end
 
   def send_weekly_digest
     DigestMailer.weekly(self.id).deliver_later
   end
-
-  # def last_weeks_episodes
-  #   episodes.where('published_at > ?', Episode.last_week_time_period[:starting_at])
-  #           .where('published_at < ?', Episode.last_week_time_period[:ending_at])
-  #           .where(dissmissed: false)
-  #           .includes(:feed)
-  # end
 
   def last_weeks_episodes
     Episode.where('published_at > ?', Episode.last_week_time_period[:starting_at])
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,35 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'model consistency' do
-    it { is_expected.to have_many(:subscriptions) }
+    it { is_expected.to have_many(:subscriptions).dependent(:destroy) }
+    it { is_expected.to have_many(:interactions).dependent(:destroy) }
     it { is_expected.to have_many(:feeds).through(:subscriptions) }
+    it { is_expected.to have_many(:episodes).through(:interactions) }
+  end
+
+  describe '.find_or_create_from_provider_data' do
+    context 'github' do
+      let!(:provider_data) { OmniAuth::AuthHash.new(provider: 'github', uid: 'abc123xyz', info: OmniAuth::AuthHash.new(email: 'jamesbond@double-o-seven.com')) }
+      context 'new user' do
+        it do
+          expect(User.find_or_create_from_provider_data(provider_data).class).to eq User
+        end
+
+        it do
+          expect(User.find_by(email: 'jamesbond@double-o-seven.com')).to eq nil
+          User.find_or_create_from_provider_data(provider_data)
+          expect(User.find_by(email: 'jamesbond@double-o-seven.com')).not_to eq nil
+          expect(User.find_by(email: 'jamesbond@double-o-seven.com').confirmed?).to eq true
+        end
+      end
+
+      context 'not new user' do
+        it do
+          user_from_first_run = User.find_or_create_from_provider_data(provider_data)
+          user_from_second_run = User.find_or_create_from_provider_data(provider_data)
+          expect(user_from_first_run).to eq user_from_second_run
+        end
+      end
+    end
   end
 end
