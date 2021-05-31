243b3ee21e3e248ca7417b62c51de4af7b049640
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-31 23:13:09 +0300

    Update factory

--- spec/factories/episodes.rb
+++ spec/factories/episodes.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :episode do
     audio_url { "https://url_of_episode_audio.mp3" }
     title { "Cool Episode" }
-    summary { "Stuff covered on this cool episode" }
     external_id { 'abc123' }
     published_at { Time.current - 3.day }
   end
