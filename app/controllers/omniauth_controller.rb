10fe7a10efe8ac750db699891bf987de1530a182
Çağrı Özkan <cagrio.dev@gmail.com>
2021-06-16 22:31:12 +0300

    Refactor

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,41 +1,30 @@
 class OmniauthController < ApplicationController
-  def github
-    @user = User.find_or_create_from_provider_data(request.env['omniauth.auth'])
+  before_action :find_or_create_user
 
-    if !@user.persisted?
-      redirect_to new_user_registration_path, alert: @user.errors.full_messages.first
-    elsif !@user.new_record?
-      sign_in(:user, @user)
-      redirect_to search_path, notice: "Welcome :^)"
-    elsif @user.new_record?
-      sign_in(:user, @user)
-      redirect_to search_path, notice: "Welcome :^) Let's start subscribing"
-    end
+  def github
+    handle_omniauth
   end
 
   def facebook
-    @user = User.find_or_create_from_provider_data(request.env['omniauth.auth'])
-    if !@user.persisted?
-      redirect_to new_user_registration_path, alert: @user.errors.full_messages.first
-    elsif !@user.new_record?
-      sign_in(:user, @user)
-      redirect_to search_path, notice: "Welcome :^)"
-    elsif @user.new_record?
-      sign_in(:user, @user)
-      redirect_to search_path, notice: "Welcome :^) Let's start subscribing"
-    end
+    handle_omniauth
   end
 
   def google_oauth2
-    @user = User.find_or_create_from_provider_data(request.env['omniauth.auth'])
+    handle_omniauth
+  end
+
+  private
+
+  def handle_omniauth
     if !@user.persisted?
       redirect_to new_user_registration_path, alert: @user.errors.full_messages.first
-    elsif !@user.new_record?
+    else
       sign_in(:user, @user)
-      redirect_to search_path, notice: "Welcome :^)"
-    elsif @user.new_record?
-      sign_in(:user, @user)
-      redirect_to search_path, notice: "Welcome :^) Let's start subscribing"
+      redirect_to porch_path
     end
+  end
+
+  def find_or_create_user
+    @user = User.find_or_create_from_provider_data(request.env['omniauth.auth'])
   end
 end
