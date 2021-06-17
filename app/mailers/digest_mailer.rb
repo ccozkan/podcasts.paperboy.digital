1569a33b4a8d51f73238baefe37b898ab86f3324
Çağrı Özkan <cagrio.dev@gmail.com>
2021-06-18 01:12:26 +0300

    Cleanup and fix minor bug

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -1,13 +1,13 @@
 class DigestMailer < ApplicationMailer
   def weekly(user_id)
-    @user = User.find_by(id: user_id)
-    episodes = @user.last_weeks_episodes.sample(3)
+    user = User.find_by(id: user_id)
+    episodes = user.last_weeks_episodes
 
     @random_episodes = episodes.sample(3)
     @number_of_new_episodes = episodes.size
 
     unless @number_of_new_episodes.zero?
-      mail(to: @user.email, subject: '🎧🚲🗞Your Weekly Podcast Digest')
+      mail(to: user.email, subject: '🎧🚲🗞Your Weekly Podcast Digest')
     end
   end
 end
