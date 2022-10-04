9bd71c7411aed84c758d02d6d75268df56f656e5
Çağrı Özkan <ccozkan@tutanota.com>
2022-10-05 01:29:27 +0300

    Add defaulturl option for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.action_mailer.default_url_options = { host: 'localhost',
+                                               port: 5100 }
 end
