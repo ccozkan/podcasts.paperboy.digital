42537b3eea6e691c3d7917c91d39b41840bf7cba
Çağrı Özkan <ccozkan@tutanota.com>
2022-10-05 01:27:54 +0300

    Add email, provider uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,30 +6,37 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:interactions).dependent(:destroy) }
     it { is_expected.to have_many(:feeds).through(:subscriptions) }
     it { is_expected.to have_many(:episodes).through(:interactions) }
+    it { is_expected.to validate_uniqueness_of(:email).scoped_to(:provider) }
   end
 
   describe '.find_or_create_from_provider_data' do
-    context 'github' do
-      let!(:provider_data) { OmniAuth::AuthHash.new(provider: 'github', uid: 'abc123xyz', info: OmniAuth::AuthHash.new(email: 'jamesbond@double-o-seven.com')) }
-      context 'new user' do
-        it do
-          expect(User.find_or_create_from_provider_data(provider_data).class).to eq User
-        end
+    let!(:provider_data) { OmniAuth::AuthHash.new(provider: 'github', uid: 'abc123xyz', info: OmniAuth::AuthHash.new(email: 'jamesbond@double-o-seven.com')) }
 
-        it do
-          expect(User.find_by(email: 'jamesbond@double-o-seven.com')).to eq nil
-          User.find_or_create_from_provider_data(provider_data)
-          expect(User.find_by(email: 'jamesbond@double-o-seven.com')).not_to eq nil
-          expect(User.find_by(email: 'jamesbond@double-o-seven.com').confirmed?).to eq true
-        end
+    context 'when new user' do
+      it 'creates a new user' do
+        expect(User.find_by(email: 'jamesbond@double-o-seven.com')).to eq nil
+        user = User.find_or_create_from_provider_data(provider_data)
+
+        expect(user.class).to eq User
+        expect(User.find_by(email: 'jamesbond@double-o-seven.com')).not_to eq nil
+        expect(User.find_by(email: 'jamesbond@double-o-seven.com').confirmed?).to eq true
       end
 
-      context 'not new user' do
-        it do
-          user_from_first_run = User.find_or_create_from_provider_data(provider_data)
-          user_from_second_run = User.find_or_create_from_provider_data(provider_data)
-          expect(user_from_first_run).to eq user_from_second_run
-        end
+      it 'creates a new user when same email is used for email-pwd user' do
+        prev_user = create(:user, email: provider_data.info.email)
+        user = User.find_or_create_from_provider_data(provider_data)
+
+        expect(prev_user).not_to eq user
+        expect(prev_user.email).to eq user.email
+      end
+    end
+
+    context 'when not new user' do
+      it 'when previous user is created from oauth' do
+        user_from_first_run = User.find_or_create_from_provider_data(provider_data)
+        user_from_second_run = User.find_or_create_from_provider_data(provider_data)
+
+        expect(user_from_first_run).to eq user_from_second_run
       end
     end
   end
