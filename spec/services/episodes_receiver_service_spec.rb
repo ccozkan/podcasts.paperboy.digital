ce94453ede1f88e9ff912c7bd3d0e5e0e6007d6f
Cagri Ozkan <cagri.ozkan@ascent.io>
2022-10-05 17:57:44 +0300

    rbucop

--- spec/services/episodes_receiver_service_spec.rb
+++ spec/services/episodes_receiver_service_spec.rb
@@ -1,24 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe EpisodesReceiverService, type: :service do
-  let(:valid_feed_url) { 'https://feeds.feedburner.com/ReverberationRadio'}
-  let(:not_feed_url) { 'https://google.com' }
-  let(:invalid_url) { 'invalid.url.hahaha' }
+  let(:valid_feed_url) { "https://feeds.feedburner.com/ReverberationRadio" }
+  let(:not_feed_url) { "https://google.com" }
+  let(:invalid_url) { "invalid.url.hahaha" }
 
-  describe 'when valid feed url' do
+  describe "when valid feed url" do
     let(:subject) { EpisodesReceiverService.new(valid_feed_url).call }
 
-    it 'returns payload and success? as true' do
+    it "returns payload and success? as true" do
       expect(subject.success?).to eq true
       expect(subject.payload.present?).to eq true
       expect(subject.error.present?).to eq false
     end
   end
 
-  describe 'when not feed url' do
+  describe "when not feed url" do
     let(:subject) { EpisodesReceiverService.new(not_feed_url).call }
 
-    it 'returns InvalidFeedUrl error and success? as false' do
+    it "returns InvalidFeedUrl error and success? as false" do
       expect(subject.success?).to eq false
       expect(subject.payload.present?).to eq false
       expect(subject.error.present?).to eq true
@@ -27,10 +27,10 @@ RSpec.describe EpisodesReceiverService, type: :service do
     end
   end
 
-  describe 'when invalid url' do
+  describe "when invalid url" do
     let(:subject) { EpisodesReceiverService.new(invalid_url).call }
 
-    it 'returns Standard error and success? as false' do
+    it "returns Standard error and success? as false" do
       expect(subject.success?).to eq false
       expect(subject.payload.present?).to eq false
       expect(subject.error.present?).to eq true
