3f9fcdae171addfe6b021c18d7786a4bf5e1d6f6
Cagri Ozkan <cagri.ozkan@ascent.io>
2022-10-05 17:28:27 +0300

    Add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_221640) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_133050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_221640) do
     t.datetime "updated_at", null: false
     t.text "last_check_error"
     t.string "slug"
+    t.boolean "healthy"
     t.index ["external_id"], name: "index_feeds_on_external_id", unique: true
     t.index ["slug"], name: "index_feeds_on_slug", unique: true
   end
