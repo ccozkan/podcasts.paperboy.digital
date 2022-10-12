97fe8f432a0b4df06fcc653eb96ed53a9bd8ac88
Çağrı Özkan <ccozkan@tutanota.com>
2022-10-12 19:21:05 +0300

    Log info when workers are running

--- app/workers/episodes_receiver_worker.rb
+++ app/workers/episodes_receiver_worker.rb
@@ -2,11 +2,16 @@ class EpisodesReceiverWorker
   include Sidekiq::Worker
 
   def perform(feed_id = nil)
+    Rails.logger.info('~~ EpisodesReceiverService has started ~~')
+    start_time = Time.now.to_i
+
     feeds = if feed_id.nil?
               Feed.all
             else
               [Feed.find_by(id: feed_id)]
             end
+
+    Rails.logger.info("~~ running for #{feeds.count} feeds")
 
     feeds.each do |feed|
       episodes = EpisodesReceiverService.new(feed.rss_url).call
@@ -26,5 +31,10 @@ class EpisodesReceiverWorker
         new_episode = Episode.create!(params)
       end
     end
+
+
+    end_time = Time.now.to_i
+    elapsed_time = end_time - start_time
+    Rails.logger.info("~~ EpisodesReceiverService has ended. Took #{elapsed_time} seconds ~~")
   end
 end
--- app/workers/weekly_digest_sender_worker.rb
+++ app/workers/weekly_digest_sender_worker.rb
@@ -2,9 +2,19 @@ class WeeklyDigestSenderWorker
   include Sidekiq::Worker
 
   def perform(*args)
+    Rails.logger.info('~~ WeeklyDigestSenderWorker has started ~~')
+    start_time = Time.now.to_i
+
     users = User.all
+
+    Rails.logger.info("~~ running for #{users.count} users")
+
     users.each do |user|
       user.send_weekly_digest
     end
+
+    end_time = Time.now.to_i
+    elapsed_time = end_time - start_time
+    Rails.logger.info("~~ WeeklyDigestSenderWorker has ended. Took #{elapsed_time} seconds ~~")
   end
 end
