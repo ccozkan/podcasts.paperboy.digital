03d5eb230606ea13b53428d26f7a22a7a0bdc573
Çağrı Özkan <ccozkan@tutanota.com>
2023-06-01 02:29:46 +0300

    wip

--- app/javascript/controllers/audio_controller.js
+++ app/javascript/controllers/audio_controller.js
@@ -2,7 +2,23 @@ import { Controller } from "@hotwired/stimulus";
 import Plyr from "plyr";
 
 export default class extends Controller {
+  static targers = ["output"]
   connect() {
     const player = new Plyr('#player');
+  }
+
+  next(event) {
+      console.log('zaa');
+      console.log(event);
+      fetch(this.audioUrl).then(
+          // console.log('heheheh')
+      );
+  }
+
+  initialize() {
+    console.log('asdsa');
+  }
+
+  reloadSource() {
   }
 }
