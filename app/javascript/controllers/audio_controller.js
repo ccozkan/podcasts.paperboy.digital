5caaeaf355f6d172a22e0215ce1502939f45db48
Çağrı Özkan <ccozkan@tutanota.com>
2023-06-01 03:27:35 +0300

    wip

--- app/javascript/controllers/audio_controller.js
+++ app/javascript/controllers/audio_controller.js
@@ -2,23 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 import Plyr from "plyr";
 
 export default class extends Controller {
-  static targers = ["output"]
-  connect() {
-    const player = new Plyr('#player');
-  }
+    static values = {
+        url: String
+    }
 
-  next(event) {
-      console.log('zaa');
-      console.log(event);
-      fetch(this.audioUrl).then(
-          // console.log('heheheh')
-      );
-  }
+    initialize() {
+        const player = new Plyr('#player');
+        console.log(this.urlValue);
+    }
 
-  initialize() {
-    console.log('asdsa');
-  }
-
-  reloadSource() {
-  }
+    next(event) {
+        var aud = document.getElementById("player");
+        aud.src = this.urlValue;
+      // console.log(aud);
+      // fetch(this.urlValue).then(
+      //     aud.src = "https://chrt.fm/track/GF1E57/traffic.megaphone.fm/TAMC2111426415.mp3?updated=1685311924"
+      // );
+        // player.play();
+    }
 }
