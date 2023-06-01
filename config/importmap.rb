83de4fdf5c6a226c17501c5d40d064da7136b4f7
Cagri Ozkan <ccozkan@tutanota.com>
2023-06-01 20:04:49 +0300

    wip

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "plyr", to: "https://ga.jspm.io/npm:plyr@3.7.3/dist/plyr.min.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.5/lib/assets/compiled/rails-ujs.js"
