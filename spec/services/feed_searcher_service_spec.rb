073beaf718c1e139eab342dff62de47b767627cb
Çağrı Özkan <ccozkan@tutanota.com>
2023-06-06 20:33:38 +0300

    Fix spec

--- spec/services/feed_searcher_service_spec.rb
+++ spec/services/feed_searcher_service_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe FeedSearcherService, type: :service do
     let(:subject) { FeedSearcherService.new("reverberation radio").call }
 
     it "should find some shows" do
-      expect(subject.length).to be > 0
+      expect(subject.payload.length).to be > 0
     end
 
     it "should return proper format" do
-      expect(subject.class).to eq Array
-      expect(subject.first.class).to eq Hash
-      expect(subject.first[:rss_url]).not_to eq nil
-      expect(subject.first[:external_id]).not_to eq nil
-      expect(subject.first[:pic_url]).not_to eq nil
-      expect(subject.first[:provider]).not_to eq nil
-      expect(subject.first[:name]).not_to eq nil
+      expect(subject.class).to eq ServiceResponse
+      expect(subject.payload.class).to eq Array
+      expect(subject.payload.first[:rss_url]).not_to eq nil
+      expect(subject.payload.first[:external_id]).not_to eq nil
+      expect(subject.payload.first[:pic_url]).not_to eq nil
+      expect(subject.payload.first[:provider]).not_to eq nil
+      expect(subject.payload.first[:name]).not_to eq nil
     end
   end
 end
