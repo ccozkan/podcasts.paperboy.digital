3a2ca74cf2aa31b032783ce98f17d4db518452cb
Çağrı Özkan <ccozkan@tutanota.com>
2023-06-09 00:48:37 +0300

    Turbofy dissmiss on porch

--- app/controllers/dismiss_episodes_controller.rb
+++ app/controllers/dismiss_episodes_controller.rb
@@ -3,10 +3,21 @@ class DismissEpisodesController < ApplicationController
 
   def update
     Interaction.dismiss!(permitted_params[:episode_id], current_user.id)
+    turbofy && return if referer_porch?
+
     redirect_to request.referer
   end
 
   private
+
+  def turbofy
+    render turbo_stream:
+             turbo_stream.remove("dismissable_episode")
+  end
+
+  def referer_porch?
+    request.referer.split('/').last == 'porch'
+  end
 
   def permitted_params
     params.permit(:episode_id)
