40ed3a3760ac0915b801556c51d8a463b38d4c4b
Çağrı Özkan <ccozkan@tutanota.com>
2023-06-12 15:10:45 +0300

    wip

--- spec/lib/simple_email_spec.rb
+++ spec/lib/simple_email_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
  
 RSpec.describe SimpleEmail, type: :model do
-  describe "validations" do
-    let(:subject) { SimpleEmail.new( {'title': 'title', 'subject': 'subject', 'from': 'feedbacker@helloword.com' } ) }
+  let(:subject) { SimpleEmail.new( {'title': 'title', 'subject': 'subject', 'from': 'feedbacker@helloword.com' } ) }
 
+  describe "validations" do
     it do
       expect(subject).to validate_presence_of(:from)
       expect(subject).to validate_presence_of(:subject)
