8cc7db1cd70d27bf4fe59d46a520774b036580d6
Çağrı Özkan <ccozkan@tutanota.com>
2023-06-13 22:06:00 +0300

    Add missing after_action lol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  after_action :record_page_view
+
   def record_page_view
     if response.content_type&.start_with?("text/html")
       ActiveAnalytics.record_request(request)
