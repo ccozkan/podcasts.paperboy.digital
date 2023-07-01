607674bac890402a4e1c31614e2f79c5f18acc94
Çağrı Özkan <ccozkan@tutanota.com>
2023-07-02 01:40:35 +0300

    Prettify

--- app/controllers/peek_feeds_controller.rb
+++ app/controllers/peek_feeds_controller.rb
@@ -1,25 +1,27 @@
 class PeekFeedsController < ApplicationController
+  before_action :find_feed, only: %i[show peekable start_peeking]
+
   def show
-    @feed = Feed.find_by(external_id: permitted_params[:external_id])
     @items = @feed.episodes.limit(5)
   end
 
   def peeking; end
 
   def peekable
-    status = Feed.find_by(external_id: permitted_params[:feed_external_id])&.episodes&.empty?
-    render json: { status: }.to_json
+    status = @feed&.episodes&.empty?
+
+    render json: { status: status }.to_json
   end
 
   def start_peeking
-    feed = Feed.find_by(external_id: permitted_params[:external_id]) || Feed.new(permitted_params)
+    feed = @feed || Feed.new(permitted_params)
 
     if feed.new_record?
       # feed.only_peek
       feed.save!
     end
 
-    redirect_to peeking_path(external_id: permitted_params[:external_id])
+    redirect_to peeking_path(external_id: feed.external_id)
   end
 
   private
@@ -28,5 +30,7 @@ class PeekFeedsController < ApplicationController
     params.permit(:external_id, :feed_rss_url, :name, :pic_url, :rss_url, :provider)
   end
 
-  def find_feed; end
+  def find_feed
+    @feed = Feed.find_by(external_id: permitted_params[:external_id])
+  end
 end
