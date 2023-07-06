09d500e2c64a39efb88f051dfee0a5e73aeb00fb
Çağrı Özkan <ccozkan@tutanota.com>
2023-07-06 21:02:39 +0300

    cleanup

--- app/controllers/random_episodes_controller.rb
+++ app/controllers/random_episodes_controller.rb
@@ -2,13 +2,8 @@ class RandomEpisodesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    episode = Episode.random_episode(permitted_params[:feed_slug])
+    episode = Episode.random_episode(params[:feed_slug])
+
     redirect_to episode_path(episode)
-  end
-
-  private
-
-  def permitted_params
-    params.permit(:feed_slug)
   end
 end
