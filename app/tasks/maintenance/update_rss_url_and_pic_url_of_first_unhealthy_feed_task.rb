c645d29c31993c278d816ecc8afbb7d2962ba557
Çağrı Özkan <ccozkan@tutanota.com>
2023-07-11 00:59:38 +0300

    Add maintenance task for updating rss url of unhealthy feeds first-by-first

---
+++ app/tasks/maintenance/update_rss_url_and_pic_url_of_first_unhealthy_feed_task.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class UpdateRssUrlAndPicUrlOfFirstUnhealthyFeedTask < MaintenanceTasks::Task
+    def collection
+      unhealthy_first_feed_id = Feed.where(healthy: false).first.id
+      Feed.where(id: unhealthy_first_feed_id)
+    end
+
+    def process(element)
+      feed = element
+      search_result = FeedSearcherService.new(feed.name).call.payload.first
+
+      if search_result[:external_id] == feed.external_id
+        new_rss = search_result[:rss_url]
+        dummy_service = EpisodesReceiverService.new(new_rss).call
+
+        raise dummy_service.error.to_s + "STOPPED! dummy service failed for #{feed.id} id, #{feed.name} error: #{dummy_service.error}" unless dummy_service.success?
+
+        feed.pic_url = search_result[:rss_url]
+        feed.rss_url = search_result[:pic_url]
+        feed.healthy = true
+        feed.save!
+      else
+        raise "STOPPED! external id match did not for #{feed.id} id, #{feed.name}"
+      end
+    end
+
+    def count; end
+  end
+end
