8c23297fa5100a1b221e3f5ada7a135627c78d17
Çağrı Özkan <ccozkan@tutanota.com>
2023-07-10 23:09:07 +0300

    Fix

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,6 +4,6 @@ class Admin::BaseController < ApplicationController
   private
 
   def check_if_user_admin?
-    redirect_to_root unless current_user.admin?
+    redirect_to root_path if current_user.nil? || !current_user.admin?
   end
 end
