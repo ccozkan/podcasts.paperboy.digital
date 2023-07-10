339efc50beb82b498854952e50509b8e147961ea
Çağrı Özkan <ccozkan@tutanota.com>
2023-07-10 23:11:01 +0300

    rubocop

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,9 +1,11 @@
-class Admin::BaseController < ApplicationController
-  before_action :check_if_user_admin?
+module Admin
+  class BaseController < ApplicationController
+    before_action :check_if_user_admin?
 
-  private
+    private
 
-  def check_if_user_admin?
-    redirect_to root_path if current_user.nil? || !current_user.admin?
+    def check_if_user_admin?
+      redirect_to root_path if current_user.nil? || !current_user.admin?
+    end
   end
 end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,13 +1,15 @@
-class Admin::DashboardController < Admin::BaseController
-  def index
-    @user_count = User.count
-    @feed_count = Feed.count
-    @episode_count = Episode.count
+module Admin
+  class DashboardController < Admin::BaseController
+    def index
+      @user_count = User.count
+      @feed_count = Feed.count
+      @episode_count = Episode.count
 
-    @last_user = User.last
-    @last_feed = Feed.last
-    @last_episode = Episode.last
+      @last_user = User.last
+      @last_feed = Feed.last
+      @last_episode = Episode.last
 
-    @unhealthy_feeds = Feed.where(healthy: false)
+      @unhealthy_feeds = Feed.where(healthy: false)
+    end
   end
 end
