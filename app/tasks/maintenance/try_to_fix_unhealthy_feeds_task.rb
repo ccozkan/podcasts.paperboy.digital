2b763be2f8b00c0663ac99aa33980d3973e63447
Çağrı Özkan <ccozkan@tutanota.com>
2023-07-11 15:57:47 +0300

    Rubocop

--- app/tasks/maintenance/try_to_fix_unhealthy_feeds_task.rb
+++ app/tasks/maintenance/try_to_fix_unhealthy_feeds_task.rb
@@ -11,30 +11,26 @@ module Maintenance
       search_results = FeedSearcherService.new(feed.name).call.payload
 
       if search_results.empty?
-        feed.last_check_error = 'FailedToFix: search_results is empty'
-        feed.save!
-        return
+        feed.last_check_error = "FailedToFix: search_results is empty"
       else
         search_result = search_results.first
-      end
 
-      if search_result[:external_id] == feed.external_id
-        new_rss = search_result[:rss_url]
-        dummy_service = EpisodesReceiverService.new(new_rss).call
+        if search_result[:external_id] == feed.external_id
+          new_rss = search_result[:rss_url]
+          dummy_service = EpisodesReceiverService.new(new_rss).call
 
-        unless dummy_service.success?
-          feed.last_check_error = "FailedToFix: Dummy EpisodesReceiverService failed for #{feed.id} id, #{feed.name} error: #{dummy_service.error}"
-          feed.save!
+          unless dummy_service.success?
+            feed.last_check_error = "FailedToFix: Dummy EpisodesReceiverService failed for #{feed.id} id, #{feed.name} error: #{dummy_service.error}"
+          end
+          feed.pic_url = search_result[:pic_url]
+          feed.rss_url = search_result[:rss_url]
+          feed.healthy = true
+        else
+          feed.last_check_error = "FailedToFix: external_id's did not match for for #{feed.id} id, #{feed.name} error: #{dummy_service.error}"
         end
-
-        feed.pic_url = search_result[:pic_url]
-        feed.rss_url = search_result[:rss_url]
-        feed.healthy = true
-        feed.save!
-      else
-        feed.last_check_error = "FailedToFix: external_id's did not match for for #{feed.id} id, #{feed.name} error: #{dummy_service.error}"
-        feed.save!
       end
+
+      feed.save!
     end
 
     def count; end
